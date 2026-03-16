c668119d8fd6b5f786a2bfd17d2de92e461cb3b6
anushkap42 <121813846+anushkap42@users.noreply.github.com>
2026-03-16 13:06:13 +0530

    Update chatbot.py

--- Task1/chatbot.py
+++ Task1/chatbot.py
@@ -1,21 +1,37 @@
+# Simple Rule-Based Chatbot
+
 print("Chatbot: Hello! I am a simple chatbot.")
 print("Type 'bye' to exit the chat.")
 
 while True:
-    user_input = input("You: ").lower()
+    user = input("You: ").lower()
 
-    if user_input == "hello" or user_input == "hi":
-        print("Chatbot: Hello! How can I help you?")
+    if user in ["hi", "hello", "hey"]:
+        print("Chatbot: Hello! Nice to meet you.")
 
-    elif "name" in user_input:
-        print("Chatbot: I am a rule-based chatbot created for the CodSoft internship.")
+    elif "how are you" in user:
+        print("Chatbot: I am doing great. Thanks for asking!")
 
-    elif "how are you" in user_input:
-        print("Chatbot: I am doing well, thank you!")
+    elif "name" in user:
+        print("Chatbot: My name is CodSoft Chatbot.")
 
-    elif user_input == "bye":
+    elif "creator" in user or "who made you" in user:
+        print("Chatbot: I was created as part of a CodSoft internship project.")
+
+    elif "time" in user:
+        import datetime
+        print("Chatbot: Current time is", datetime.datetime.now().strftime("%H:%M:%S"))
+
+    elif "date" in user:
+        import datetime
+        print("Chatbot: Today's date is", datetime.date.today())
+
+    elif "help" in user:
+        print("Chatbot: You can ask me about my name, time, date, or say hello.")
+
+    elif user == "bye":
         print("Chatbot: Goodbye! Have a nice day.")
         break
 
     else:
-        print("Chatbot: Sorry, I don't understand that.")
+        print("Chatbot: Sorry, I didn't understand that.")
